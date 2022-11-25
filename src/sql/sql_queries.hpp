43ea2f60e7f7b5d25d90633e885a4682e5a4c6fa
prr133f <prr133f@margined.studio>
2022-11-25 17:46:23 +0300

    фыфыфы

--- src/sql/sql_queries.hpp
+++ src/sql/sql_queries.hpp
@@ -3,17 +3,18 @@
 #include "userver/storages/postgres/query.hpp"
 namespace queries {
 const userver::storages::postgres::Query get_query{
-    "SELECT * FROM $1", userver::storages::postgres::Query::Name{"get_query"}};
+    "SELECT * FROM hello_schema.users",
+    userver::storages::postgres::Query::Name{"get_query"}};
 
 const userver::storages::postgres::Query post_query{
-    "INSERT INTO $1(name, count) VALUES($2, $3)",
+    "INSERT INTO hello_schema.users(name, count) VALUES($1, $2)",
     userver::storages::postgres::Query::Name{"post_query"}};
 
 const userver::storages::postgres::Query put_query{
-    "UPDATE $1 SET count = $2 WHERE name = $3",
+    "UPDATE hello_schema.users SET count = $1 WHERE name = $2",
     userver::storages::postgres::Query::Name{"put_query"}};
 
 const userver::storages::postgres::Query delete_query{
-    "DELETE FROM $1 WHERE name = $2",
+    "DELETE FROM hello_schema.users WHERE name = $1",
     userver::storages::postgres::Query::Name{"delete_query"}};
 }  // namespace queries
